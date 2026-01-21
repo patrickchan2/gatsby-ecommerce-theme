ec8f316a6ea8c029a49f4f7b05bbb1d136ede5c6
Patrick Chan <patrickcyc@gmail.com>
2026-01-21 08:43:56 +0800

    update brand name

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -10,7 +10,7 @@ const Brand = (props) => {
       role={'presentation'}
       onClick={() => navigate('/')}
     >
-      {/* <h4>SYDNEY</h4> */}
+      {/* <h4>Kidubaby</h4> */}
       <svg
         width={127}
         height={24}
